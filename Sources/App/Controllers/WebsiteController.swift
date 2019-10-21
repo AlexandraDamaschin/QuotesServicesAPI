f62bcd597d5ea43fac06c1c556a838ac25e4df57
Mihai Pantiru <mihai.pantiru@gmail.com>
2019-10-21 12:14:08 +0300

    Implement add new quote

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -1,20 +1,34 @@
 import Vapor
 import Leaf
 
-// 1
 struct WebsiteController: RouteCollection {
-    // 2
     func boot(router: Router) throws {
-        // 3
         router.get(use: indexHandler)
+        router.get("quote", "create", use: addQuotePageHandler)
+        router.post(
+          Quote.self, at: "quote", "create",
+          use: addQuoteHandler)
     }
     
-    // 4
     func indexHandler(_ req: Request) throws -> Future<View> {
         let quotes = Quote.query(on: req).all()
         let context = QuotesContext(title: "All Quotes", quotes: quotes)
-        // 5
+        
         return try req.view().render("index", context)
+    }
+    
+    func addQuotePageHandler(_ req: Request) throws -> Future<View>{
+        return try req.view().render("addPage")
+    }
+    
+    func addQuoteHandler(_ req: Request, quote: Quote) throws -> Future<Response> {
+      return quote.save(on: req).map(to: Response.self) { acronym in
+        guard let id = acronym.id else {
+          throw Abort(.internalServerError)
+        }
+        return req.redirect(to: "/")
+        
+      }
     }
 }
 
